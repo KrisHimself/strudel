bad03395b5d39e4708a2a30b8a7d486331746eee
KRistmets <56702831+KrisHimself@users.noreply.github.com>
2025-12-18 18:18:12 +0200

    Fix left-hand and right-hand notes in viimaneKlaver.js

--- viimaneKlaver.js
+++ viimaneKlaver.js
@@ -12,17 +12,17 @@ let hands = {
   rh3: "[C4 D4 C4 D4, E4] [C4 D4 C4 D4, G4]",
   lh3: "A2, F3 E3",
   rh4: "[Bb3 D4 Bb3 D4, A4] [Bb3 D4 Bb3 D4, G4 F4]",
-  lh4: "G2, D3 - G3 F3",
+  lh4: "G2, D3 F3",
   rh5: "[A3 C4 A3 C4, G4] [A3 C4 A3 C4, E4 F4]",
-  lh5: "F2, G3 - E3 F3",
+  lh5: "F2, G3@2 E3 F3",
   rh6: "[G3 Bb3 G3 Bb3, F4 - - Eb4] [G3 Bb3 G3 Bb3, D4 C4]",
-  lh6: "Eb2 Bb2, F3",
-  rh7: "[D4 C4 D4 C4, G4] [D4 C4, Gb4]",
+  lh6: "Eb2, Eb3 Bb2",
+  rh7: "[A3 G3 A3 G3, D4] [Gb3, D4 C4]",
   lh7: "D2@1.5 A2@0.5 D2@2, D3 D3",
-  rh8: "[A3 B3 A3 B3, D4] [G3, D4 - E4@2]",
+  rh8: "A3 B3 A3 B3 G3@4, D4@3 E4@1",
   lh8: "G2, D3",
 }
- 
+
 piano: "<msr1 msr2 msr3 msr4 msr5 msr6 msr7 msr8>".pickRestart({
   msr1: `<rh1, lh1>`,
   msr2: `<rh2, lh2>`,
